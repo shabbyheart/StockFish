80c5c20dd528e5d12d1bdb1239b3714449885947
Omar  Faruq <omar.faruq@asthait.com>
2025-04-29 09:57:55 +0600

    project initiated

---
+++ StockFish.WebAPI/Controllers/EngineController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StockFish.WebAPI.Controllers.Base;
+
+namespace StockFish.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EngineController : BaseApiController
+    {
+        //[HttpGet("best-move")]
+        //[ProducesResponseType(typeof(BestMoveDTO), 200)]
+        //[OpenApiOperation("get best move", "")]
+        //[AllowAnonymous]
+        /*public async Task<IActionResult> GetBestMove([FromQuery] GetBestMoveRequest request)
+        {
+            var responseDTO = await Mediator.Send(request);
+
+            return Ok(responseDTO);
+        }*/
+    }
+}
